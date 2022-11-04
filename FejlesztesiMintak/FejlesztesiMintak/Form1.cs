7a629ba1d934715e92654e40fb9cc5136cade17a
dorkad <dorka.danyis@gmail.com>
2022-11-04 13:06:13 +0100

    c2

--- FejlesztesiMintak/FejlesztesiMintak/Form1.cs
+++ FejlesztesiMintak/FejlesztesiMintak/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FejlesztesiMintak.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,19 @@ namespace FejlesztesiMintak
 {
     public partial class Form1 : Form
     {
+        private List<Ball> _balls = new List<Ball>();
+        private BallFactory _factory;
+
+        public BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            Factory = new BallFactory();
         }
     }
 }
